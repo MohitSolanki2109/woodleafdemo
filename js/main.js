7c0f440ca586000d90231ab78a9c4ce021d8ae2c
Mohit Solanki <solankimohit15093@gmail.com>
2026-04-08 10:42:00 +0530

    Remove code protection comments from main.js
    Removed code protection comments and adjusted formatting.

--- js/main.js
+++ js/main.js
@@ -202,12 +202,12 @@
     });
   }
 
-  /* ---- CODE PROTECTION ---- 
+  /* ---- CODE PROTECTION ---- */
   document.addEventListener("contextmenu", e => e.preventDefault());
   document.addEventListener("keydown", e => {
     if (e.key === "F12" || (e.ctrlKey && e.shiftKey && ["I","J","C"].includes(e.key)) || (e.ctrlKey && e.key === "U")) {
       e.preventDefault();
     }
   });
-*/
+
 })();
